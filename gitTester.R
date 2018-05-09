abc28b52be4e6d4ad72daf0b455a95ead9426ada
Eric Taylor <ericgibsontaylor@gmail.com>
2018-05-09 11:42:33 -0700

    init proposing project to Merran

---
+++ gitTester.R
@@ -0,0 +1,7 @@
+# Hey Merran, I thought we could test out using GIT together with this script. I will send you a
+# link on GitHub.
+# This can be a longer term/slow project but I though it would be fun to test.
+
+# I will create a repo on GitHub with this file. I beleive you will need to make a "pull request"
+# This would result in your downloading the file. Make some changes (i.e. respond to this message)
+# Then I think you somehow submit your changes and I would accept them.
